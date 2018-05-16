b30daabae7c9cccf0d0b792e944880bad658fc6d
Sergheev Andrian <sergheev@icloud.com>
2018-05-16 11:15:59 +0300

    Scheme&ViewControllers is done.

---
+++ RestaurantApp/Models/MenuItem.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  RestaurantApp
+//
+//  Created by Sergheev Andrian on 5/16/18.
+//  Copyright © 2018 Sergheev Andrian. All rights reserved.
+//
+
+import Foundation
+
+
+struct MenuItem : Codable {
+    
+    
+    
+}
---
+++ RestaurantApp/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TabBarViewController.swift
+//  RestaurantApp
+//
+//  Created by Sergheev Andrian on 5/16/18.
+//  Copyright © 2018 Sergheev Andrian. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Success")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
