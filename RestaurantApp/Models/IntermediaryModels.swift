eb4ae686b8de71db5ffe11e53811fed17418ff75
Sergheev Andrian <sergheev@icloud.com>
2018-05-16 11:46:55 +0300

    app models were created.

---
+++ RestaurantApp/Models/IntermediaryModels.swift
@@ -0,0 +1,32 @@
+//
+//  IntermediaryModels.swift
+//  RestaurantApp
+//
+//  Created by Sergheev Andrian on 5/16/18.
+//  Copyright © 2018 Sergheev Andrian. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+//MARK: Intermediary models. API returns a list of categories under the key categories. Similarly to decoding the data that contains menu items you'll need an intermediate object.
+
+struct Categories : Codable{
+    let categories: [String]
+}
+
+
+//MARK: Intermediary models. API returns a value, preparation_time, returned from the /order endpoint representing the amount of time until the order would be ready.
+
+struct PreparationTime :  Codable {
+    
+    let prepTime: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case prepTime = "preparation_time"
+        
+    }
+}
+
--- RestaurantApp/Models/MenuItem.swift
+++ RestaurantApp/Models/MenuItem.swift
@@ -9,8 +9,59 @@
 import Foundation
 
 
+
+
+
+
 struct MenuItem : Codable {
     
     
+    var id: Int
+    var name: String
+    var description: String
+    var price: Double
+    var category: String
+    var imageURL: URL
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case description
+        case price
+        case category
+        case imageURL = "image_url"
+    }
+    
+    
+    
+    //MARK: We are not using init(from:) here hence the custom keys are decoded properly so there is no need for a custom decode/implementation here.
+    
     
 }
+
+
+//MARK: Additional layer since the JSON API sends an array of items to us:
+/*
+ {
+ "items" : [
+ 
+ ]
+ }
+ */
+struct MenuItems: Codable {
+    let items: [MenuItem]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
